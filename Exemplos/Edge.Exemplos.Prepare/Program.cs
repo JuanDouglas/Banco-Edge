c9bd21373073c28c978f19203ffbca75a38af0fe
Juan Douglas <juandouglas2004@gmail.com>
2022-06-01 14:00:10 -0300

    Alterado para pessoas

--- Exemplos/Edge.Exemplos.Prepare/Program.cs
+++ Exemplos/Edge.Exemplos.Prepare/Program.cs
@@ -2,14 +2,14 @@
 using System.Data.SQLite;
 using System.Diagnostics;
 
-const string insertCommand = "INSERT INTO [Carros](Nome,Preco) VALUES(@nome,@preco)";
+const string insertCommand = "INSERT INTO [Pessoas](Nome,Email,CPF) VALUES(@nome,@email,@cpf)";
 string pathRecursos = $@"{Environment.CurrentDirectory}\Recursos";
 string pathData = @$"{pathRecursos}\Database.db";
 string cs = $@"Data Source={pathData};New=true";
 bool created = true;
 Stopwatch stopwatch = new();
 
-Carro[] carros = JsonConvert.DeserializeObject<Carro[]>(File.ReadAllText($@"{pathRecursos}\Carros.json")) ?? Array.Empty<Carro>();
+Pessoa[] pessoas = JsonConvert.DeserializeObject<Pessoa[]>(File.ReadAllText($@"{pathRecursos}\Pessoas.json")) ?? Array.Empty<Pessoa>();
 
 if (!File.Exists(pathData))
     created = false;
@@ -19,23 +19,23 @@ con.Open();
 
 using var cmd = new SQLiteCommand(insertCommand, con);
 stopwatch.Start();
-cmd.Prepare();
 
 if (!created)
 {
-    cmd.CommandText = "CREATE TABLE [Carros](" +
+    cmd.CommandText = "CREATE TABLE [Pessoas](" +
         "[Nome] VARCHAR(500)," +
-        "[Preco] MONEY);";
+        "[Email] VARCHAR(500)," +
+        "[CPF] VARCHAR(15));";
     cmd.ExecuteScalar();
 
     cmd.CommandText = insertCommand;
-    cmd.Prepare();
 }
 
-foreach (var carro in carros)
+foreach (var carro in pessoas)
 {
-    cmd.Parameters.AddWithValue("@nome", carro.Marca);
-    cmd.Parameters.AddWithValue("@preco", carro.Preco);
+    cmd.Parameters.AddWithValue("@nome", carro.Nome);
+    cmd.Parameters.AddWithValue("@email", carro.Email);
+    cmd.Parameters.AddWithValue("@cpf", carro.CPF);
     cmd.ExecuteNonQuery();
 }
 
@@ -43,8 +43,9 @@ stopwatch.Stop();
 Console.WriteLine($"Tempo de Inserção: {stopwatch.Elapsed}");
 
 
-class Carro
+class Pessoa
 {
-    public string Marca { get; set; }
-    public decimal Preco { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string CPF { get; set; }
 }
